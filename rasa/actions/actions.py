374fc7aead2adc544af07ea285c4cbe77bfeb335
Akshata19 <akshatamithari@gmail.com>
2025-04-11 12:36:43 +0200

    Added two chatbot versions

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,27 +1,20 @@
-# This files contains your custom actions which can be used to run
-# custom Python code.
-#
-# See this guide on how to implement these action:
-# https://rasa.com/docs/rasa/custom-actions
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
 
+class ActionLogTicket(Action):
+    def name(self) -> Text:
+        return "action_log_ticket"
 
-# This is a simple example for a custom action which utters "Hello World!"
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+        order_id = next(tracker.get_latest_entity_values("order_id"), None)
+
+        if not order_id:
+            dispatcher.utter_message(response="utter_ask_again")
+        else:
+            dispatcher.utter_message(response="utter_ticket_logged")
+
+        return []
